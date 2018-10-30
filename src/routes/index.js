9d3a777480b555b65b83d597075b89f567617bf4
Jorge Madson <filholouco@gmail.com>
2018-10-29 21:29:59 -0300

    trocando o class por className

--- src/routes/index.js
+++ src/routes/index.js
@@ -50,27 +50,27 @@ const About = () => (
 );
 
 const Login = () => (
-  <div class="mdl-cell mdl-cell--4-col">
-    <div class="mdl-layout mdl-js-layout mdl-color--grey-100">
-      <main class="mdl-layout__content">
-        <div class="mdl-card mdl-shadow--6dp">
-          <div class="mdl-card__title mdl-color--primary mdl-color-text--white">
-            <h2 class="mdl-card__title-text">Login</h2>
+  <div className="mdl-cell mdl-cell--4-col">
+    <div className="mdl-layout mdl-js-layout mdl-color--grey-100">
+      <main className="mdl-layout__content">
+        <div className="mdl-card mdl-shadow--6dp">
+          <div className="mdl-card__title mdl-color--primary mdl-color-text--white">
+            <h2 className="mdl-card__title-text">Login</h2>
           </div>
-          <div class="mdl-card__supporting-text">
+          <div className="mdl-card__supporting-text">
             <form action="#">
-              <div class="mdl-textfield mdl-js-textfield">
-                <input class="mdl-textfield__input" type="text" id="username" />
-                <label class="mdl-textfield__label" for="username">Username</label>
+              <div className="mdl-textfield mdl-js-textfield">
+                <input className="mdl-textfield__input" type="text" id="username" />
+                <label className="mdl-textfield__label" htmlFor="username">Username</label>
               </div>
-              <div class="mdl-textfield mdl-js-textfield">
-                <input class="mdl-textfield__input" type="password" id="userpass" />
-                <label class="mdl-textfield__label" for="userpass">Password</label>
+              <div className="mdl-textfield mdl-js-textfield">
+                <input className="mdl-textfield__input" type="password" id="userpass" />
+                <label className="mdl-textfield__label" htmlFor="userpass">Password</label>
               </div>
             </form>
           </div>
-          <div class="mdl-card__actions mdl-card--border">
-            <button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Log in</button>
+          <div className="mdl-card__actions mdl-card--border">
+            <button className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Log in</button>
           </div>
         </div>
       </main>
